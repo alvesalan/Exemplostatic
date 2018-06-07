bc96f54e528f520f2ec49ec52ba203b17e6be3d6
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-06-07 15:20:21 -0300

    exemplo01

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Animal lula = new Animal();
+            Animal tota = new Animal();
+            Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public static int UltimoCodigo = 0;
+        public int Codigo;
+
+        public Animal()
+        {
+            Codigo = UltimoCodigo;
+            UltimoCodigo++;
+        }
+        }
+    }
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ExemploStatic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class ExemploStatic
+    {
+        static void Main(string[] args)
+        {
+            Animal lula = new Animal();
+            Animal tota = new Animal();
+        }
+    }
+
+    class Animal
+    {
+        public static int UltimoCodigo = 0;
+        public int Codigo;
+
+        public Animal()
+        {
+            Codigo = UltimoCodigo;
+            UltimoCodigo++;
+        }
+    }
+}
